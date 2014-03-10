3e83ebc74b850889c42aa4de97192bd68fa93b5f
Etienne Cella <etienne.p.cella@gmail.com>
2014-03-10 11:27:09 -0400

    start working on automaton

--- js/automaton.js
+++ js/automaton.js
@@ -1,6 +1,45 @@
 // automaton is responsible for applying rules on cells,
 // exposing cell states, but not rendering them
-var Automaton = function(){
 
+function genCells(height_, width_) {
+
+	// init cells
+	var cells = [],
+		i = 0,
+		nCells = height_ * width_;
+	for (i; i < nCells; ++i) cells[i] = {
+		state: 0
+	};
+
+	// ref neighbors
+	var neighborIndexes = [-1, -width_ - 1, // top left 
+		-1, -width_, // top
+		-1, -width_ + 1, // top right
+		0, -1, // left
+		0, 1, // right
+		1, width_ - 1, // bottom left 
+		1, width_, // bottom
+		1, width_ + 1 // bottom right
+	],
+		neighbors = null,
+		cell = null,
+		j = 0;
+	for (i = 0; i < nCells; ++i) {
+
+		neighbors = [];
+		for (j = 0; j < 8; ++j) {
+
+			// expected line
+
+			// expected index
+		}
+	}
+	return cells;
+}
+
+var Automaton = function(height_, width_) {
+
+
+	var self = {};
 
 }
