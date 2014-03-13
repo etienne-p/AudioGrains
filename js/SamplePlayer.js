a26818aa5ab6e9ea863f6ef8abb3276b22c04265
Etienne Cella <etienne.p.cella@gmail.com>
2014-03-13 17:39:11 -0400

    start working on granulator

--- js/SamplePlayer.js
+++ js/SamplePlayer.js
@@ -3,8 +3,7 @@ lib.SamplePlayer = function(left, right) {
 	var pos = 0,
 		len = left.length,
 		rate = 1,
-		amp = 1,
-		cRate = 1;
+		amp = 1;
 
 	// interpolation requires accessing samples at potentially Math.ceil((len - 1).546))
 	// make sure that this sample exists
@@ -13,21 +12,17 @@ lib.SamplePlayer = function(left, right) {
 	left[len] = left[0];
 	right[len] = right[0];
 
-	function processAudio(e) {
+	function processAudio(outBufferL, outBufferR) {
 
-		var outBufferL = e.outputBuffer.getChannelData(0),
-			outBufferR = e.outputBuffer.getChannelData(1),
-			bufLen = outBufferL.length,
-			l, r,
+		var bufLen = outBufferL.length,
 			alpha = 0; // interpolation
 
 		for (var i = 0; i < bufLen; ++i) {
 			alpha = pos - Math.floor(pos);
-			outBufferL[i] = l = amp * 0.5 * (alpha * left[Math.floor(pos)] + (1 - alpha) * left[Math.ceil(pos)]);
-			outBufferR[i] = r = amp * 0.5 * (alpha * right[Math.floor(pos)] + (1 - alpha) * right[Math.ceil(pos)]);
-			pos = (len + pos + ((rate - cRate) * (i / bufLen) + cRate)) % len;
+			outBufferL[i] = amp * 0.5 * (alpha * left[Math.floor(pos)] + (1 - alpha) * left[Math.ceil(pos)]);
+			outBufferR[i] = amp * 0.5 * (alpha * right[Math.floor(pos)] + (1 - alpha) * right[Math.ceil(pos)]);
+			pos = (len + pos + rate) % len;
 		}
-		cRate = rate;
 	};
 
 	return {
---
+++ js/granulator.js
@@ -0,0 +1,110 @@
+// ARCHITECTURE: use a single sample player to genrate grains dynamically
+// hope i don't burn the cpu ;p
+
+var GranulatorUtil = (function() {
+
+	// used to generate grain envelopes
+	// x in [0 - 1]
+	function sinEnv(x) {
+		return Math.cos(Math.PI * (2 * x + 1));
+	}
+
+	// returns an array
+	function genEnv(length) {
+		var i = 0,
+			rv = [],
+			l = length - 1;
+		for (; i < length) rv[i] = sinEnv(i / l);
+		return rv;
+	}
+
+	// supports ANY delay
+	function addGrainToBuffer(grainLeft, grainRight, bufLeft, bufRight, delay) {
+		var grainLen = grainLeft.length,
+			bufLen = bufLeft.length,
+			i = delay < 0 ? -delay : 0,
+			len = Math.max(0, Math.min(bufLen - delay, grainLen) - i);
+
+		// sample min: delay max: delay + grainLen buffer min: 0 max: bufLen
+		for (; i < len; ++i) {
+			bufLeft[delay + i] += grainLeft[i];
+			bufRight[delay + i] += grainRight[i];
+		}
+	}
+
+	// rates: grains pitches
+	// delays: grain delays (in samples)
+	// posRatios: grains sample atrt ratio
+	// sampler: a sample player responsible for providing audio at proper rate
+	function addGrainsToBuffer(grains, rates, delays, posRatios, sampler, bufLeft, bufRight) {
+
+		var count = rates.length,
+			i = 0,
+			grain = null;
+		for (; i < count; ++i) {
+			grain = grains[i];
+			sampler.rate(rates[i]);
+			sampler.posRatio(posRatios[i]);
+			sampler.processAudio(grain.l, grain.r);
+			addGrainToBuffer(grain.l, grain.r, bufLeft, bufRight, delays[i]);
+		}
+	}
+
+	// generate empty buffers, we'll reuse these buffers to limit gc
+	function genEmptyGrains(count, length) {
+		var i = 0,
+			rv = [],
+			emptyBuffer = [];
+
+		for (; i < length¸; ++i) emptyBuffer[i] = 0;
+
+		for (i = 0; i < count; ++i) {
+			rv[i] = {
+				l: emptyBuffer.slice(0),
+				r: emptyBuffer.slice(0)
+			}
+		}
+		rv;
+	}
+
+	return function() {
+		this.genEmptyGrains = genEmptyGrains;
+		this.addGrainsToBuffer = addGrainsToBuffer;
+		return this;
+	};
+})();
+
+var Granulator = function(count, length, sampler) {
+	this.grains = this.genEmptyGrains();
+	this.sampler = sampler;
+}
+
+Granulator.prototype = {
+
+	constructor: Granulator,
+
+	requestFrameData: function() {
+		throw 'requestFrameData should be overriden';
+	}
+
+	onProcessAudio: function(e) {
+
+		// The output buffer contains the samples that will be modified and played
+		var outputBufferL = e.outputBuffer.getChannelData(0),
+			outputBufferR = e.outputBuffer.getChannelData(1),
+			frameData = this.requestFrameData(); // request data
+
+		// addGrainsToBuffer(grains, rates, delays, posRatios, sampler, bufLeft, bufRight) {
+		this.addGrainsToBuffer(
+			this.grains,
+			frameData.rates,
+			frameData.delays,
+			frameData.posRatios,
+			this.sampler,
+			outputBufferL,
+			outputBufferR);
+	}
+}
+
+// use functional mixin
+GranulatorUtil.call(Granulator.prototype);
